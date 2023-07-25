697e0e456fe8c8a342c6940b0f3500d38769a7f4
lalithamadupalli <lalithamadupalli@gmail.com>
2023-07-25 15:59:52 +0530

    app.js

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -4,35 +4,19 @@ import {
   withAuthenticator, 
   Button, 
   Heading, 
-
   Image, 
-
   View, 
-
   Card, 
-
 } from "@aws-amplify/ui-react"; 
 
- 
-
 function App({ signOut }) { 
-
   return ( 
-
     <View className="App"> 
-
       <Card> 
-
         <Image src={logo} className="App-logo" alt="logo" /> 
-
         <Heading level={1}>We now have Auth!</Heading> 
-
       </Card> 
-
       <Button onClick={signOut}>Sign Out</Button> 
-
     </View> 
-
-  ); 
-
+  );
 } 
